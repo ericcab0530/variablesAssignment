d964ffada87eed0a4528aef6524aa0f35be7fd8d
ericccab0530 <ecabrera22@mail.valenciacollege.edu>
2024-09-02 18:54:08 -0400

    Inital push

---
+++ main.py
@@ -0,0 +1,21 @@
+#This program will solve a series of calculations
+answer = ("The answer to your query is")
+mult = (5 * 6)
+print(answer, mult)
+
+div = ("%.8f" % (8 / 3))
+print(answer, div)
+
+divR = (8 // 3)
+print(answer, divR)
+
+mod = (8 % 3)
+print(answer, mod)
+
+add = (4200000 + 23090)
+print(answer, add)
+
+sub = (4200000 - 7633)
+print(answer, sub)
+
+print("Eric Cabrera")
